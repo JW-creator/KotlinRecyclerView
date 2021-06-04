04b2dab128e1c783a3800f2e427203d9b4137dde
JW-creator <jasonwan_js@hotmail.com>
2021-06-03 22:46:37 -0500

    Initial commit

---
+++ app/src/main/java/com/example/kotlinrecyclerview/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.kotlinrecyclerview
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val contacts = createContacts()
+        rvContacts.adapter = ContactAdapter(this, contacts)
+        rvContacts.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun createContacts(): List<Contact> {
+        val contacts = mutableListOf<Contact>()
+        for (i in 1..150) contacts.add(Contact("Person #$i", i))
+        return contacts
+    }
+}
